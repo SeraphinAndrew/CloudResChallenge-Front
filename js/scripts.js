99ad9ed73fdd85d68b605c44413db91c3df0137d
SeraphinAndrew <109246506+SeraphinAndrew@users.noreply.github.com>
2022-08-14 23:21:09 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@
 */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -32,3 +32,27 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+// GET API REQUEST
+async function get_visitors() {
+    // call post api request function
+    //await post_visitor();
+    try {
+        let response = await fetch('https://ej8kgrssn9.execute-api.us-east-1.amazonaws.com/crcprod/counter', {
+            method: 'GET',
+            headers: {
+                //'x-api-key': 'JslbDfdt1F8fl7wE4CRIj1Oqidmtmzqw4lZ539Sj',
+            }
+        });
+        let data = await response.json()
+        document.getElementById("visitors").innerHTML = data['count'] + " visits to this page.";
+        console.log(data);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+
+get_visitors();
